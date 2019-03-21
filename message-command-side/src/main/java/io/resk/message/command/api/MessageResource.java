0e886173f917be38aec2231f8a26bb798d40c115
Julius Krah <juliuskrah@gmail.com>
2019-03-21 12:46:41 +0000

    Added JWT security

--- message-command-side/src/main/java/io/resk/message/command/api/MessageResource.java
+++ message-command-side/src/main/java/io/resk/message/command/api/MessageResource.java
@@ -9,20 +9,21 @@ import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.security.annotation.Secured;
 import io.resk.message.command.vm.Message;
 import lombok.extern.slf4j.Slf4j;
 
-// https://fcm.googleapis.com/v1/{parent=projects/*}/messages:send
 // https://firebase.google.com/docs/cloud-messaging/send-message
 // https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages/send
 @Slf4j
+@Secured("${resk.routes.secured}")
 @Controller("/projects")
 public class MessageResource {
 
 	@Version("1")
 	@Post("{projectId}/messages:send")
 	public HttpStatus send(String projectId, @Body Message message, @Nullable Principal principal) {
-		log.info("Project v1: {}, Payload: {}", projectId, message);
+		log.info("Principal: {}, Project v1: {}, Payload: {}", principal, projectId, message);
 		return HttpStatus.ACCEPTED;
 	}
 
--- message-command-side/src/test/groovy/io/resk/message/command/api/MessageResourceSpec.groovy
+++ message-command-side/src/test/groovy/io/resk/message/command/api/MessageResourceSpec.groovy
@@ -1,11 +1,16 @@
 package io.resk.message.command.api
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpHeaders
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.security.authentication.UsernamePasswordCredentials
+import io.micronaut.security.token.jwt.render.BearerAccessRefreshToken
 import io.resk.message.command.vm.Message
 import spock.lang.AutoCleanup
 import spock.lang.Shared
@@ -20,16 +25,56 @@ class MessageResourceSpec extends Specification {
 	@AutoCleanup
 	RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
 	
-	def "Test Message Endpoint"() {
-		when: "Creating a new message"
-		HttpRequest request = HttpRequest.POST("/projects/sda/messages:send", 
-				new Message(null, null, null, null, null, null, null, null, null))
-				.headers({ headers -> headers.add("X-API-VERSION", "1") })
-				
-		HttpResponse<String> rsp = client.toBlocking()
-			.exchange(request)
+	def "Test Message Endpoint when Authentication is Enabled"() {
+		Message message = new Message(null, null, null, null, null, null, null, null, null)
+
+		when: 'Accessing a secured URL without authentication'
+		  def request = HttpRequest.POST("/projects/sda/messages:send", message)
+			.headers({ headers -> headers.add("X-API-VERSION", "1") })
+			
+	      client.toBlocking().exchange(request)
+	
+	    then: "returns unauthorized"
+	      HttpClientResponseException e = thrown(HttpClientResponseException)
+	      e.status == HttpStatus.UNAUTHORIZED
+		  
+		when: "Login endpoint is called with valid credentials"
+		  UsernamePasswordCredentials creds = new UsernamePasswordCredentials("julius", "password")
+		  request = HttpRequest.POST('/login', creds)
+		  HttpResponse<BearerAccessRefreshToken> rsp = client.toBlocking().exchange(request, BearerAccessRefreshToken)
+		  
+		then: "token is returned"
+		  rsp.status == HttpStatus.OK
+		  
+	    when: "Using token in next request to create message"
+		  String accessToken = rsp.body().accessToken
 		
-		then: "Return accepted"
-		rsp.status == HttpStatus.ACCEPTED
+		  request = HttpRequest.POST("/projects/sda/messages:send", message)
+				.headers(
+					{ headers -> 
+					  headers.add("X-API-VERSION", "1") 
+					  headers.add(HttpHeaders.AUTHORIZATION, "Bearer $accessToken")
+					})
+				
+		  HttpResponse<String> resp = client.toBlocking().exchange(request)
+		
+		then: "returns accepted"
+		  resp.status == HttpStatus.ACCEPTED
+		
+		when: "Creating a new message with versioning in parameter"
+		  def uri = UriBuilder.of('/projects/{projectId}/messages:send')
+		  			      .queryParam("api-version", "1")
+						  .expand(Collections.singletonMap("projectId", "demo"))
+		  request = HttpRequest.POST(uri, message).header(HttpHeaders.AUTHORIZATION, "Bearer $accessToken")
+				
+		  resp = client.toBlocking().exchange(request)
+		
+		then: "returns accepted"
+		  resp.status == HttpStatus.ACCEPTED
 	}
+	
+	def "Test Message Endpoint when Authentication is Disabled"() {
+		
+	}
+
 }
