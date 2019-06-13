b159e169f2f219ab709bf16cfb684de105feb895
Julius Krah <juliuskrah@gmail.com>
2019-06-13 14:27:57 +0000

    source for micronaut issue micronaut-projects/micronaut-core#1791

--- src/main/java/com/juliuskrah/GenericController.java
+++ src/main/java/com/juliuskrah/GenericController.java
@@ -11,18 +11,14 @@ import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.uri.UriBuilder;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
-@Slf4j
 @RequiredArgsConstructor
 public abstract class GenericController<T, ID extends Serializable> {
     private final StatusService<T> service;
     public abstract String getPath();
     @Post
     public Mono<HttpResponse<T>> save(@Valid @Body T entity) {
-        log.info("entity '{}'",  entity);  // com.fasterxml.jackson.databind.node.ObjectNode
-
         return service.save(entity).map(savedEntity -> {
             var location = UriBuilder.of(getPath()) //
                             .path("1").build();
--- src/main/java/com/juliuskrah/StatusController.java
+++ src/main/java/com/juliuskrah/StatusController.java
@@ -2,8 +2,12 @@ package com.juliuskrah;
 
 import java.util.UUID;
 
+import javax.validation.Valid;
+
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.validation.Validated;
+import reactor.core.publisher.Mono;
 
 @Validated
 @Controller(StatusController.PATH)
@@ -15,6 +19,11 @@ public class StatusController extends GenericController<Status, UUID> {
     @Override
     public String getPath() {
         return PATH;
+    }
+
+    @Override
+    public Mono<HttpResponse<Status>> save(@Valid Status entity) {
+        return super.save(entity);
     }
 
 }
